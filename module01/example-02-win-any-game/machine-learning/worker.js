34c3cf7acf0ccd2145a35402fd9c5c14b3133a08
Lara <laraamori.machado@gmail.com>
2026-08-12 20:47:27 -0300

    lint

--- module01/example-02-win-any-game/machine-learning/worker.js
+++ module01/example-02-win-any-game/machine-learning/worker.js
@@ -3,18 +3,16 @@ importScripts('https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@latest');
 const MODEL_PATH = `yolov5n_web_model/model.json`;
 const LABELS_PATH = `yolov5n_web_model/labels.json`;
 
-
 self.onmessage = async ({ data }) => {
-    if (data.type !== 'predict') return
+  console.log('worker onmessage');
+  if (data.type !== 'predict') return;
 
-    postMessage({
-        type: 'prediction',
-        x: 400,
-        y: 400,
-        score: 0
-    });
-
-
+  postMessage({
+    type: 'prediction',
+    x: 400,
+    y: 400,
+    score: 0,
+  });
 };
 
 console.log('🧠 YOLOv5n Web Worker initialized');
